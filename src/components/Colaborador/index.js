914b67127f933daebf431612646beeba9abc695b
Maria Eduarda Oliveira <oliveiradelimamariaeduarda@gmail.com>
2023-08-07 15:13:44 -0300

    feat: create collaborator card

---
+++ src/components/Colaborador/index.js
@@ -0,0 +1,17 @@
+import "./Colaborador.css";
+
+const Colaborador = () => {
+  return (
+    <div className="colaborador">
+      <div className="cabecalho">
+        <img src="http://github.com/M-Eduarda-Oliveira.png" alt="Dudui"/>
+      </div>
+      <div className="rodape">
+        <h4>Duda Oliveira</h4>
+        <h5>Estagigata</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Colaborador;
